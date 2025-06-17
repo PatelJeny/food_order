77bf0cd35a1524e87e3138463a1172fe43dbef32
jeny patel <pateljeny1509@gmail.com>
2025-06-17 12:41:20 +0530

    ...

---
+++ admin/manage-admin.php
@@ -0,0 +1,18 @@
+<?php
+include('partials/menu.php');
+?>
+
+ <!-- main content section start  -->
+ <div class="main-content">
+    <div class="wrapper">
+        <h1>Manage Admin</h1>
+    
+
+
+    </div>
+</div>
+ <!-- main content section end  -->
+
+ <?php
+ include('partials/footer.php');
+ ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,12 @@
+<!-- Footer section start  -->
+
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">2020 All rights reserved , House of Taste . developed by - <a href="#">Jeny patel</a></p>
+    </div>
+</div>
+ <!-- Footer section end  -->
+
+    
+</body>
+</html>
